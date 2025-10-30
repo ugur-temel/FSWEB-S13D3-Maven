b58708f63fd255787293a063a76cc9fb2012c829
ugur-temel <temelu091@gmail.com>
2025-10-30 14:30:43 +0300

    tasks completed

---
+++ src/main/java/org/example/Wall.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Wall {
+    public double width;
+    public double height;
+
+    public Wall(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth(){
+        return  width;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setWidth(double width){
+        if(width<0){
+            this.width = 0.0;
+        }else {
+            this.width = width;
+        }
+    }
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0.0;
+        } else {
+            this.height = height;
+        }
+    }
+    public double getArea(){
+        return width*height;
+    }
+}
+
